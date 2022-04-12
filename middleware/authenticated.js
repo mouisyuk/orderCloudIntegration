2155abbc6cfafe8d18f118cfa775de233799d0a5
Ilya Mouisyuk <i.mouisyuk@brimit.com>
2022-04-12 04:29:15 +0300

    Fix: deploy issue

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,14 +1,14 @@
 import { Tokens, Auth } from 'ordercloud-javascript-sdk';
-import { useMeStore } from '~/store';
+import { useAuthenticationStore } from '~/store';
 import jwt_decode from "jwt-decode";
 
 export default async () => {
     try {
         if(!Tokens.GetAccessToken()) {
-            const meStore = useMeStore();
+            const authenticationStore = useAuthenticationStore();
             const response = await Auth.Anonymous(process.env.PUBLIC_OC_CLIENT_ID, process.env.PUBLIC_OC_SCOPE.split(','));
-            meStore.isAnonymous = true;
-            meStore.decodedToken = jwt_decode(response.access_token);
+            authenticationStore.isAnonymous = true;
+            authenticationStore.decodedToken = jwt_decode(response.access_token);
             
             Tokens.SetAccessToken(response.access_token);
             Tokens.SetRefreshToken(response.refresh_token);
